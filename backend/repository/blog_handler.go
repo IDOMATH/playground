8c1025c8f6a9c7964e2eac3a81aad70b3b445297
IDOMATH <btdothemath@gmail.com>
2026-01-20 21:32:31 -0600

    Stubbing get blogs

--- backend/repository/blog_handler.go
+++ backend/repository/blog_handler.go
@@ -5,3 +5,7 @@ import "net/http"
 func (repo *Repository) HandleCreateBlogPost(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 }
+
+func (repo *Repository) HandleGetBlogPosts(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
