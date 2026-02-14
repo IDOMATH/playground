c41c9518da6b3a4874c5758ff771ae9459fc701e
IDOMATH <btdothemath@gmail.com>
2026-02-13 22:52:28 -0600

    Update repository.go

--- backend/repository/repository.go
+++ backend/repository/repository.go
@@ -5,3 +5,7 @@ import "github.com/idomath/playground/backend/db"
 type Repository struct {
 	BlogStore db.BlogStore
 }
+
+func NewRepository(bs db.Blogstore) Repository {
+	return Repository{Blogstore: bs}
+}
