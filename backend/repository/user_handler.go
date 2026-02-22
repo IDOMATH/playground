99fa6b244a0cbe0f74234cbdf6e64baef18dd2e9
IDOMATH <btdothemath@gmail.com>
2026-02-21 21:14:22 -0600

    logout stub

--- backend/repository/user_handler.go
+++ backend/repository/user_handler.go
@@ -9,3 +9,5 @@ func (repo *Repository) HandleNewUser(w http.ResponseWriter, r *http.Request) {
 func (repo *Repository) HandleLogin(w http.ResponseWriter, r *http.Request) {
 
 }
+
+func (repo *Repository) HandleLogout(w http.ResponseWriter, r *http.Request) {}
