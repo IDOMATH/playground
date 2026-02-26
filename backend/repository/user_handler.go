3e8213319eeff390479a1428212f0c8723faf087
IDOMATH <btdothemath@gmail.com>
2026-02-25 23:25:10 -0600

    insert user

--- backend/repository/user_handler.go
+++ backend/repository/user_handler.go
@@ -1,9 +1,25 @@
 package repository
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/idomath/playground/backend/models"
+)
 
 func (repo *Repository) HandleNewUser(w http.ResponseWriter, r *http.Request) {
 
+	var user *models.User
+	err := json.NewDecoder(r.Body).Decode(user)
+	if err != nil {
+		// log.Error("HandlePostNewUser", "error decoding user to json from body", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("getting user from body"))
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Inserted new user"))
 }
 
 func (repo *Repository) HandleLogin(w http.ResponseWriter, r *http.Request) {
