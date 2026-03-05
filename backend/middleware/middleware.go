a582733179ee6ecd3c20f71009558ba5c126f156
IDOMATH <btdothemath@gmail.com>
2026-03-04 19:41:29 -0600

    Authorize start

--- backend/middleware/middleware.go
+++ backend/middleware/middleware.go
@@ -1,5 +1,29 @@
 package middleware
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/idomath/playground/backend/repository"
+)
 
 type Middleware func(http.HandlerFunc) http.HandlerFunc
+
+func Authorize(repo *repository.Repository) Middleware {
+	return func(next http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			_, found := repo.Session.Get(r.Header.Get("cheetauth"))
+			if !found {
+				fmt.Println("NOT AUTHENTICATED")
+				// Potentially do some rerouting if the endpoint is protected
+				w.WriteHeader(http.StatusUnauthorized)
+				w.Write([]byte(r.Header.Get("cheetauth")))
+				return
+			}
+
+			w.Write([]byte("Authorized"))
+
+			next(w, r)
+		}
+	}
+}
