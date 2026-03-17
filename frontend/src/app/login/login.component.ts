1365e761c355f24acee5215c44848dc361b0bf94
IDOMATH <btdothemath@gmail.com>
2026-03-16 21:29:41 -0500

    Login form setup

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+let initialEmailValue = '';
 
 @Component({
   selector: 'app-login',
   imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
-
+  form = new FormGroup({
+    email: new FormControl(initialEmailValue, {
+      validators: [Validators.email, Validators.required],
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)],
+    }),
+  });
 }
